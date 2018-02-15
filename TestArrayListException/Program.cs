1a775f53ed7e694b970741391744d86e8d0534fd
Oleg Kozintsev <o.kozintsev@gmail.com>
2018-02-15 14:54:04 +0400

    add TestArrayListException
    This causes an InvalidCastException to be returned.

---
+++ TestArrayListException/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace TestArrayListException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            // Add an integer to the list.
+            list.Add(3);
+            // Add a string to the list. This will compile, but may cause an error later.
+            list.Add("It is raining in Redmond.");
+
+            var t = 0;
+            // This causes an InvalidCastException to be returned.
+            foreach (int x in list)
+            {
+                t += x;
+            }
+        }
+    }
+}
